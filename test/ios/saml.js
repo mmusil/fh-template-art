7f6c35850b57b1e3f3d6a5990c285c43178055bd
Jan Hellar <jhellar@redhat.com>
2017-04-03 10:57:15 +0200

    Use webviewcontext for saml ios test

--- test/ios/saml.js
+++ test/ios/saml.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const appium = require('../../utils/appium');
+
 function test() {
 
   const self = this;
@@ -8,12 +10,11 @@ function test() {
     return self.driver
       .elementByName("Sign In").click()
       .sleep(10000)
-      .elementByXPath("//XCUIElementTypeApplication[1]/XCUIElementTypeWindow[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeWebView[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[3]/XCUIElementTypeTextField[1]")
-        .sendKeys("student")
-      .elementByXPath("//XCUIElementTypeApplication[1]/XCUIElementTypeWindow[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeWebView[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[5]/XCUIElementTypeSecureTextField[1]")
-        .sendKeys("studentpass")
-      .elementByXPath("//XCUIElementTypeApplication[1]/XCUIElementTypeWindow[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeWebView[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[1]/XCUIElementTypeOther[6]/XCUIElementTypeButton[1]")
-        .click()
+      .then(() => appium.webviewContext(self.driver))
+      .elementByCss('#username').sendKeys("student")
+      .elementByCss('#password').sendKeys("studentpass")
+      .elementByCss('input[type="submit"]').click()
+      .context('NATIVE_APP')
       .sleep(5000)
       .elementByName("Great! You're signed in.").text().should.become("Great! You're signed in.");
   });
