fb54d709470a99d8d3144a5558de0193fbf1488e
Jan Hellar <jhellar@redhat.com>
2017-04-19 12:49:42 +0200

    Fixed test for Android Cordova SAML app

--- test/cordova/saml.js
+++ test/cordova/saml.js
@@ -10,23 +10,39 @@ function test() {
     return self.driver
       .elementByCss('.sign-in-button').click()
       .sleep(10000)
-      .then(() => self.driver.contexts())
-      .then(contexts => self.driver.context(contexts[2]))
-      .elementByCss('#username').sendKeys("student")
-      .elementByCss('#password').sendKeys("studentpass")
-      .elementByCss('#regularsubmit').isDisplayed()
-      .then(visible => {
-        if (visible) {
-          return self.driver.elementByCss('#regularsubmit').click();
+      .then(() => {
+        if (self.platform === 'android') {
+          return self.driver
+            .context('NATIVE_APP')
+            .elementByXPath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.webkit.WebView[1]/android.webkit.WebView[1]/android.view.View[1]/android.view.View[5]/android.view.View[2]/android.widget.EditText[1]")
+              .sendKeys("student")
+            .elementByXPath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.webkit.WebView[1]/android.webkit.WebView[1]/android.view.View[1]/android.view.View[5]/android.view.View[5]/android.widget.EditText[1]")
+              .sendKeys("studentpass")
+            .elementByXPath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.webkit.WebView[1]/android.webkit.WebView[1]/android.view.View[1]/android.view.View[5]/android.view.View[3]/android.widget.Button[1]")
+              .click()
+            .sleep(10000)
+            .elementByXPath("//android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[3]/android.widget.Button[1]")
+              .click();
         } else {
-          return self.driver.elementByCss('#mobilesubmit').click();
+          return self.driver.contexts()
+            .then(contexts => self.driver.context(contexts[2]))
+            .elementByCss('#username').sendKeys("student")
+            .elementByCss('#password').sendKeys("studentpass")
+            .elementByCss('#regularsubmit').isDisplayed()
+            .then(visible => {
+              if (visible) {
+                return self.driver.elementByCss('#regularsubmit').click();
+              } else {
+                return self.driver.elementByCss('#mobilesubmit').click();
+              }
+            })
+            .context('NATIVE_APP')
+            .sleep(10000)
+            .alertText().should.eventually.include('Great, you\'re signed in!')
+            .dismissAlert()
+            .then(() => appium.webviewContext(self.driver));
         }
-      })
-      .context('NATIVE_APP')
-      .sleep(10000)
-      .alertText().should.eventually.include('Great, you\'re signed in!')
-      .dismissAlert()
-      .then(() => appium.webviewContext(self.driver));
+      });
   });
 
 }
